7d1c5b7fb2cdc57b12c9871f7f1ca274a5b2d82a
Scott Stroz <scott.stroz@oracle.com>
2024-10-21 11:43:46 -0400

    Added Faker

--- Node/Node_Test_Runner/test/user-repository.test.js
+++ Node/Node_Test_Runner/test/user-repository.test.js
@@ -1,10 +1,10 @@
-import { test, before, after } from 'node:test';
-import { strict as assert } from 'node:assert';
-import {MySqlContainer} from "testcontainers";
-import { ddl } from "../setup/ddl.js";
-import { testUtils } from "../utils/testUtils.js";
-import UserRepo from "../repository/user-repository.js";
-import DataUtils from "../utils/DataUtils.js";
+import { test, before, after } from 'node:test'
+import { strict as assert } from 'node:assert'
+import {MySqlContainer} from "testcontainers"
+import { faker } from '@faker-js/faker'
+import { ddl } from "../setup/ddl.js"
+import UserRepo from "../repository/user-repository.js"
+import DataUtils from "../utils/DataUtils.js"
 
 test('Testing User Repository', async (t) => {
     let container;
@@ -34,16 +34,17 @@ test('Testing User Repository', async (t) => {
         await container.stop();
     })
     await t.test('Container should be running', async (t)=>{
-        const queryResult = await container.executeQuery("SELECT 1 as res");
+        const queryResult = await container.executeQuery("SELECT 1 as res")
         assert.equal(queryResult,"res\n1\n" )
     });
 
     await t.test('Should create user', async(t)=>{
-        const name = testUtils.generateString(50);
-        await userRepo.createUser({name: name});
-        const queryResult = await dataUtils.getUserByName(name);
-        assert.equal(1, queryResult.length);
-        assert.equal(name, queryResult[0].name);
+        const name= faker.internet.userName()
+        console.log(`Test name: ${name}.`)
+        await userRepo.createUser({name: name})
+        const queryResult = await dataUtils.getUserByName(name)
+        assert.equal(1, queryResult.length)
+        assert.equal(name, queryResult[0].name)
     })
   
-  }); 
+  })
