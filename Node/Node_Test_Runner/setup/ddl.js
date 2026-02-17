add51ac094a46c643936a068d6aaff58ab1a38d5
Scott Stroz <scott.stroz@oracle.com>
2026-02-17 09:34:00 -0500

    Refactored SQL string formatting in `createUserTable` for improved readability.

--- Node/Node_Test_Runner/setup/ddl.js
+++ Node/Node_Test_Runner/setup/ddl.js
@@ -1,6 +1,9 @@
 const ddl = {
     async createUserTable(session) {
-        const sql = "CREATE TABLE IF NOT EXISTS `user` (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(100) NOT NULL, PRIMARY KEY (id))";
+        const sql = "CREATE TABLE IF NOT EXISTS `user` " +
+            "(id INT NOT NULL AUTO_INCREMENT, " +
+            "name VARCHAR(100) NOT NULL, " +
+            "PRIMARY KEY (id))";
         const query = await session.sql(sql).execute();
         session.close;
     }
